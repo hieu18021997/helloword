bec6aa072e691b9d3ad17246a646453474280b4f
hieu18021997 <hieu1997.me@gmail.com>
2022-05-02 01:07:33 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const winston = require('winston')
 const expressWinston = require('express-winston')
-
 require('winston-daily-rotate-file');
+
+const serviceId = Math.floor(Math.random() * 900) + 100
+const servicesUUID = require('uuid').v4()
+
 console.log = require('./config/logger')
 app.use(expressWinston.logger(require('./config/expressLogger')))
 
@@ -15,19 +18,30 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 let requestId = 0
+app.use('/now', function(req,res) {
+    res.send({
+        serviceId : serviceId,
+        servicesUUID : servicesUUID,
+        requestId : requestId++,
+        method : req.method,
+        query : req.query,
+        body : req.body,
+    })
+})
+
 app.use('/', function (req, res) {
     let resTime = (Math.floor(Math.random() * 5) + 0) * 500
     setTimeout(()=>{
         res.send({
+            serviceId : serviceId,
+            servicesUUID : servicesUUID,
             requestId : requestId++,
-            pid : process.pid,
             method : req.method,
             query : req.query,
             body : req.body,
             resTime
         })
-    }, resTime)
-    
+    }, resTime)  
 })
 
 app.listen(process.env.PORT || 8080,()=>{
